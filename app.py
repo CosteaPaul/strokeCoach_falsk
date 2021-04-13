917718ec05d6625a712b6f4802d72569fb744b3e
Paul Costea <paul.igor.costea@embl.de>
2021-04-13 20:52:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def upload_file():
+def upload():
    return render_template('upload.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
